d67617dfbe443040b54635ec6f107eec846da64a
Nicolas Pires Simão <rm90173@estudante.fieb.edu.br>
2025-02-26 12:29:15 -0300

    anim_incompleta_video_8

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,15 +9,65 @@ public class PlayerController : MonoBehaviour
 {
 
     public float walkSpeed;
+    public float runSpeed;
     Vector2 moveInput;
 
-    public bool IsMoving { get; private set; }
+    public float CurrentMoveSpeed{get
+    {
+        if(IsMoving)
+        {
+            if(IsRunning)
+            {
+                return runSpeed;
+            }
+            else
+            {
+                return walkSpeed;
+            }
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    }
+
+    [SerializeField]
+    private bool _isMoving = false;
+
+
+    public bool IsMoving { get
+    {
+        return _isMoving;
+    } 
+    private set
+    {
+        _isMoving = value;
+        animator.SetBool("isMoving",value);
+    }
+    }
+    
+    [SerializeField]
+    private bool _isrunning = false;
+    public bool IsRunning
+    {
+        get
+        {
+            return _isrunning;
+        }
+        set
+        {
+            animator.SetBool("isRunning", value);
+        }
+    }
 
     Rigidbody2D rb;
+    Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Start()
@@ -32,18 +83,40 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
     }
 
-     public void OnMove(InputAction.CallbackContext context)
+    public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
 
         IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
     }
 
-    void OnAttack()
+    private void SetFacingDirection(Vector2 MoveInput)
     {
+        if(moveInput.x > 0)
+        {
 
+        }
+        if(moveInput.x < 0)
+        {
+
+        }
     }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            IsRunning = true;
+        }
+        else if (context.canceled)
+        {
+            IsRunning = false;
+        }
+    }
+   
 }
