9411a708e4b7b35d4f49c29d78dd6ff2fe4c8d64
Caio Marinho Galvão <rm90310@estudante.fieb.edu.br>
2025-03-25 12:30:35 -0300

    Ataque vídeo 13 8 minutos

---
+++ Assets/Inimigo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour
+{
+    public float walkSpeed = 3f;
+    Rigidbody2D rb;
+
+    private void Awake(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate(){
+        rb.velocity = new Vector2(walkSpeed * Vector2.right.x, rb.velocity.y);
+    }
+}
