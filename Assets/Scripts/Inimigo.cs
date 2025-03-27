cf04c5766fd25e36a63ec48ca3c7644ab9592e3e
Caio Marinho Galvão <rm90310@estudante.fieb.edu.br>
2025-03-27 12:14:56 -0300

    attack vídeo 13 completo

---
+++ Assets/Scripts/Inimigo.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirection))]
+public class Inimigo : MonoBehaviour
+{
+    public float walkSpeed = 3f;
+    Rigidbody2D rb;
+    TouchingDirection touchingDirection;
+    public enum WalkableDirection {Right, Left};
+    private WalkableDirection _walkDirection;
+    public Vector2 walkDirectionVector = Vector2.right;
+    
+    public WalkableDirection WalkDirection
+    {
+        get {return _walkDirection;}
+        set{
+            if(_walkDirection != value)
+            {
+                gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x *-1, gameObject.transform.localScale.y);
+                if(value == WalkableDirection.Right)
+                {
+                    walkDirectionVector = Vector2.right;
+                } else if(value == WalkableDirection.Left)
+                {
+                    walkDirectionVector = Vector2.left;
+                }
+            }
+            _walkDirection = value; }
+        
+    }
+    private void Awake(){
+        rb = GetComponent<Rigidbody2D>();
+        touchingDirection = GetComponent<TouchingDirection>();
+    }
+
+    private void FixedUpdate(){
+        if(touchingDirection.IsGround && touchingDirection.IsOnWall)
+        {
+            FlipDirection();
+        }
+        rb.velocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.velocity.y);
+    }
+
+    private void FlipDirection()
+    {
+        if(WalkDirection == WalkableDirection.Right)
+        {
+            WalkDirection = WalkableDirection.Left;
+        } else if(WalkDirection == WalkableDirection.Left)
+        {
+            WalkDirection = WalkableDirection.Right;
+        } else 
+        {
+            Debug.LogError("Current walkable direction is not set to legal valuer of right or left");
+        }
+    }
+}
