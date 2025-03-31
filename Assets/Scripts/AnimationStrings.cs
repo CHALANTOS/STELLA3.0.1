b916df27702d16fffe1d26693894981ab2eb89a3
Caio Marinho Galvão <rm90310@estudante.fieb.edu.br>
2025-03-31 12:32:15 -0300

    Vídeo 15 Imcompleto - 9:10 minutos, Requisitar Sprites Stella e Inimigo demaiando/morrendo

--- Assets/Scripts/AnimationStrings.cs
+++ Assets/Scripts/AnimationStrings.cs
@@ -14,8 +14,9 @@ internal class AnimationStrings : MonoBehaviour
     internal static string yVelocity = "yVelocity";
     internal static string jump = "jump";
     internal static string isOnWall = "isOnWall";
-    internal static string isOnCeiling = "isOnCeiling";
+    internal static string isOnCeiling = "isOnCeiling";s
     internal static string attackTrigger = "attack";
     internal static string canMove = "canMove";
     internal static string HasTarget = "HasTarget";
+    internal static string isAlive = "isAlive";
 } 
